f4b44e47142608d7f98e34dfeb0994c22efb6304
Apion <masonbell09@gmail.com>
2025-03-08 16:47:58 -0500

    Respect failed commands when installing ble.sh. Add neovim config. Add fzf integration to .blerc

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 ln -f $PWD/.bashrc $HOME/.bashrc
 ln -f $PWD/.bash_aliases $HOME/.bash_aliases
 ln -f $PWD/.bash_profile $HOME/.bash_profile
-ln -f $PWD/.inputrc $HOME/.inputrc
-wget -O - https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz | tar xJf -
-bash ble-nightly/ble.sh --install ~/.local/share
+ln -f $PWD/.blerc $HOME/.blerc
+wget -O - https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz | tar xJf - && 
+bash ble-nightly/ble.sh --install ~/.local/share &&
 rm -rf ble-nightly
