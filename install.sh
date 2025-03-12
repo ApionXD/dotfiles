58168b704c4cfd498822aecba3e3f9353d864f85
Apion <masonbell09@gmail.com>
2025-03-12 18:20:13 -0400

    Better install scripts

--- install.sh
+++ install.sh
@@ -2,7 +2,5 @@
 ln -f $PWD/.bashrc $HOME/.bashrc
 ln -f $PWD/.bash_aliases $HOME/.bash_aliases
 ln -f $PWD/.bash_profile $HOME/.bash_profile
-ln -f $PWD/.blerc $HOME/.blerc
-wget -O - https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz | tar xJf - && 
-bash ble-nightly/ble.sh --install ~/.local/share &&
-rm -rf ble-nightly
+mkdir -p $HOME/.config/nvim/
+ln -f $PWD/.config/nvim/init.lua $HOME/.config/nvim/init.lua
