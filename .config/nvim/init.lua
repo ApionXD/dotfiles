c02c7a372317beafd47bb39781494e9be1ef5c0d
apion <masonbell09@gmail.com>
2025-03-16 16:21:01 -0700

    Added a bunch of nvim plugins, including more completions, colorscheme, and file browser

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,6 +21,9 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.g.coq_settings = { auto_start = 'shut-up' }
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 -- Setup lazy.nvim
@@ -31,16 +34,26 @@ require("lazy").setup({
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
         "neovim/nvim-lspconfig",
-	"ms-jpq/coq_nvim"
+        "ms-jpq/coq_nvim",
+        "nvim-tree/nvim-tree.lua",
+        "ms-jpq/coq.thirdparty",
+        "ms-jpq/coq.artifacts",
+        "folke/tokyonight.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "ibhagwan/fzf-lua"
     }
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "tokyonight" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = true }
 })
-
+require("nvim-tree").setup()
+require("coq_3p") {
+    { src="nvimlua" },
+    { src="repl" }
+}
 require("mason").setup()
 require("mason-lspconfig").setup()
 local lspconfig = require("lspconfig")
@@ -48,3 +61,4 @@ local coq = require("coq")
 lspconfig.kotlin_language_server.setup(coq.lsp_ensure_capabilities())
 lspconfig.bashls.setup(coq.lsp_ensure_capabilities())
 lspconfig.lua_ls.setup(coq.lsp_ensure_capabilities())
+vim.cmd("colorscheme tokyonight")
