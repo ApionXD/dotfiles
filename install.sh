b387eb832dd456f370413f04c404de61fe73d9c6
apion <masonbell09@gmail.com>
2025-03-16 16:33:07 -0700

    Install blesh conf regardless of whether blesh is installed

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 ln -f $PWD/.bashrc $HOME/.bashrc
 ln -f $PWD/.bash_aliases $HOME/.bash_aliases
 ln -f $PWD/.bash_profile $HOME/.bash_profile
+ln -f $PWD/.blerc $HOME/.blerc
 mkdir -p $HOME/.config/nvim/
 ln -f $PWD/.config/nvim/init.lua $HOME/.config/nvim/init.lua
 echo "Dotfiles installed"
--- install_blesh.sh
+++ install_blesh.sh
@@ -1,4 +1,3 @@
-ln -f $PWD/.blerc $HOME/.blerc
 wget -O - https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz | tar xJf - && 
 bash ble-nightly/ble.sh --install ~/.local/share &&
 rm -rf ble-nightly
