f28a137e0e61a6d5b6f602f9909a289fa9abcac4
apion <masonbell09@gmail.com>
2025-03-16 16:25:10 -0700

    Add success message to install script

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -f $PWD/.bash_aliases $HOME/.bash_aliases
 ln -f $PWD/.bash_profile $HOME/.bash_profile
 mkdir -p $HOME/.config/nvim/
 ln -f $PWD/.config/nvim/init.lua $HOME/.config/nvim/init.lua
+echo "Dotfiles installed"
