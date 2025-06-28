8b893565bcfa085ae9f0d1f1b7df6ba9d6e2e553
Apion <masonbell09@gmail.com>
2025-06-28 13:42:26 -0400

    Add latest neovim config from work laptop

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -20,45 +20,84 @@ vim.opt.rtp:prepend(lazypath)
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-vim.g.coq_settings = { auto_start = 'shut-up' }
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
+vim.opt.foldlevel = 99
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    -- add your plugins here
-    {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "neovim/nvim-lspconfig",
-        "ms-jpq/coq_nvim",
-        "nvim-tree/nvim-tree.lua",
-        "ms-jpq/coq.thirdparty",
-        "ms-jpq/coq.artifacts",
-        "folke/tokyonight.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "ibhagwan/fzf-lua"
-    }
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "tokyonight" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true }
+    rocks = {
+        enabled = false
+    },
+    spec = {
+        -- add your plugins here
+        {
+            {
+                "saghen/blink.cmp",
+                version = "1.*",
+                opts = {
+                    keymap = {
+                        preset = 'enter'
+                    },
+                    completion = {
+                        documentation = {
+                            auto_show = false
+                        }
+                    }
+                }
+            },
+            {
+                "folke/trouble.nvim",
+                opts = {},
+                cmd = "Trouble"
+            },
+            {
+                "williamboman/mason.nvim",
+                opts = {}
+            },
+            {
+                "mason-org/mason-lspconfig.nvim",
+                opts = {},
+                dependencies = {
+                    { "mason-org/mason.nvim", opts = {} },
+                    "neovim/nvim-lspconfig",
+                },
+            },
+            { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} },
+            "mfussenegger/nvim-dap",
+            "neovim/nvim-lspconfig",
+            "nvim-tree/nvim-tree.lua",
+            "folke/tokyonight.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "ibhagwan/fzf-lua",
+            "kevinhwang91/nvim-ufo",
+            "kevinhwang91/promise-async"
+        }
+    },
+    -- Configure any other settings here. See the documentation for more details.
+    -- colorscheme that will be used when installing plugins.
+    install = { colorscheme = { "tokyonight" } },
+    -- automatically check for plugin updates
+    checker = { enabled = true }
 })
+
 require("nvim-tree").setup()
-require("coq_3p") {
-    { src="nvimlua" },
-    { src="repl" }
+require('ufo').setup()
+require('dapui').setup()
+
+local capabilities = {
+    textDocument = {
+        foldingRange = {
+            dynamicRegistration = false,
+            lineFoldingOnly = true
+        }
+    }
 }
-require("mason").setup()
-require("mason-lspconfig").setup()
-local lspconfig = require("lspconfig")
-local coq = require("coq")
-lspconfig.kotlin_language_server.setup(coq.lsp_ensure_capabilities())
-lspconfig.bashls.setup(coq.lsp_ensure_capabilities())
-lspconfig.lua_ls.setup(coq.lsp_ensure_capabilities())
+capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
+vim.lsp.config('*', {
+    capabilities = capabilities
+})
+
 vim.cmd("colorscheme tokyonight")
